2356f0857cea21cb764d465de4023b52e072a7eb
IBimsShweit <72552171+IBimsShweit@users.noreply.github.com>
2020-11-13 12:22:15 +0100

    Email system

--- assets/sendmail/sendmail.php
+++ assets/sendmail/sendmail.php
@@ -1,9 +1,9 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 
-require_once 'phpmailer/Exception.php';
-require_once 'phpmailer/PHPMailer.php';
-require_once 'phpmailer/SMTP.php';
+require_once 'phpmailer/src/Exception.php';
+require_once 'phpmailer/src/PHPMailer.php';
+require_once 'phpmailer/src/SMTP.php';
 
 $mail = new PHPMailer(true);
 
